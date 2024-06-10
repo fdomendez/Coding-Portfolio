c7101ff7754d27f3628a92a3e1cb8cd066481cde
Fernando Méndez Díaz <fdomendez@outlook.com>
2024-06-10 09:50:35 -0600

    update

--- LeetCode/obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
+++ LeetCode/obj/Debug/net8.0/LeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b09cfe37465ede4b4d1bac18733b9462eadd6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4425f1e4f92c9af293f4a98c2663199264e991")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
